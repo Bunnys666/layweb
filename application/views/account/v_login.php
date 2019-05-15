75c9d8963f88392a6b28819237d7e0b0ad108ab6
unknown <muhammadryanapriyudan@gmail.com>
2019-05-15 16:38:18 +0700

    Login

--- application/views/account/v_login.php
+++ application/views/account/v_login.php
@@ -6,9 +6,19 @@
    <title>
      Login
    </title>
- </head>
+   <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="../assets/css/bootstrap.js">
+   <link rel="stylesheet" href="../assets/css/bootstrap.min.js">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIbrEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
  <body>
-      <h2>Halaman Login</h2>
+      
       <?php
    // Cetak jika ada notifikasi
       if($this->session->flashdata('sukses')) {
@@ -16,27 +26,40 @@
       }
       ?>
  
+ <style>
+     .a{
+          position:relative;
+     }
+     .b{
+          position:absolute;
+          top:40%;
+          left:50%;
+          transform:translate(-50%,-50%);
+     }
+ </style>
+      <div style="height:100vh;" class="a">
+      <div class="b" > <br>
       <?php echo form_open('login');?>
-      <p>Username:</p>
-      <p>
-           <input type="text" name="username" value="<?php echo set_value('username'); ?>"/>
-      </p>
-      <p> <?php echo form_error('username'); ?> </p>
- 
-      <p>Password:</p>
-      <p>
-           <input type="password" name="password" value="<?php echo set_value('password'); ?>"/>
-      </p>
-      <p> <?php echo form_error('password'); ?> </p>
- 
-      <p>
-           <input type="submit" name="btnSubmit" value="Login" />
-      </p>
- 
+      <form action="" class="text-center border border-light p-5">
+      <p class="h4 mb-4">Sign in</p>
+
+           <input type="text" name="username"  value="<?php echo set_value('username'); ?>" id="defaultLoginFormName" class="form-control mb-4" placeholder="username"/>
+           
+           <input type="password" name="password" value="<?php echo set_value('password'); ?>" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="password"/>
+       
+           <button class="btn btn-info btn-block my-4" type="submit" value="Login">Sign in</button>
+
       <?php echo form_close();?>
- 
+  
       <p>
-           Kembali ke beranda, Silakan klik <?php echo anchor(site_url().'/beranda','di sini..'); ?>
+           Kembali ke beranda, Silahkan klik <?php echo anchor(site_url().'/beranda','di sini..'); ?>
       </p>
+      </form>
+
+     
+
+      </div>
+      </div>
+      
  </body>
  </html>
--- application/views/account/v_register.php
+++ application/views/account/v_register.php
@@ -6,49 +6,51 @@
    <title>
      blabla
    </title>
+   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="../assets/css/bootstrap.js">
+   <link rel="stylesheet" href="../assets/css/bootstrap.min.js">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIbrEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
  </head>
  <body>
-     <h2>Pendaftaran Akun</h2>
- 
+   
+ <style>
+     .a{
+          position:relative;
+     }
+     .b{
+          position:absolute;
+          top:47%;
+          left:50%;
+          transform:translate(-50%,-50%);
+     }
+ </style>
+
+   <div style="height:100vh;" class="a"> 
+   <div class="b">
+
+      <form action="" class="text-center border border-light p-5">
      <?php echo form_open('register');?>
-     <p>Nama:</p>
-     <p>
-     <input type="text" name="name" value="<?php echo set_value('name'); ?>"/>
-     </p>
-     <p> <?php echo form_error('name'); ?> </p>
- 
-     <p>Username:</p>
-     <p>
-     <input type="text" name="username" value="<?php echo set_value('username'); ?>"/> 
-     </p>
-     <p> <?php echo form_error('username'); ?> </p>
- 
-     <p>Email:</p>
-     <p>
-     <input type="text" name="email" value="<?php echo set_value('email'); ?>"/>
-     </p>
-     <p> <?php echo form_error('email'); ?> </p>
- 
-     <p>Password:</p>
-     <p>
-     <input type="password" name="password" value="<?php echo set_value('password'); ?>"/>
-     </p>
-     <p> <?php echo form_error('password'); ?> </p>
- 
-     <p>Password Confirm:</p>
-     <p>
-     <input type="password" name="password_conf" value="<?php echo set_value('password_conf'); ?>"/>
-     </p>
-     <p> <?php echo form_error('password_conf'); ?> </p>
- 
-     <p>
-     <input type="submit" name="btnSubmit" value="Daftar" />
+     <p class="h4 mb-4">Sign up</p>
+     
+        <input class="form-control mb-4"  type="text" placeholder="Nama" name="name" value="<?php echo set_value('name'); ?>" required/>
+        <input class="form-control mb-4"  type="text" name="username" placeholder="Username" id="defaultLoginFomUserName" value="<?php echo set_value('username'); ?>" required/> 
+        <input class="form-control mb-4"  type="text" name="email" placeholder="E-mail" value="<?php echo set_value('email'); ?>" required/>
+        <input class="form-control mb-4"  type="password" name="password" placeholder="Password" value="<?php echo set_value('password'); ?>" required/>
+     
+        <button class="btn btn-success btn-block my-4" type="submit" value="Daftar">Sign up</button>
+
+  
+        <p>
+     Kembali ke beranda, Silakan klik <?php echo anchor(site_url().'/beranda','di sini..'); ?>
      </p>
  
      <?php echo form_close();?>
- 
-     <p>
-     Kembali ke beranda, Silakan klik <?php echo anchor(site_url().'/beranda','di sini..'); ?>
-     </p>
+     </form>
+ </div>
+ </div>
  </body>
  </html>
